ae37504f32d239747d9a97c3d14e4bf45f17cfb0
papiex <boutdepapier27@outlook.fr>
2021-09-08 09:23:58 +0200

    Adding error management player tournament creation

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 from model import Tournament, Player
-from data import PLAYERS, PLAYERS_NUMBERS, get_field_data
+from data import PLAYERS
 
 
 class Controller:
@@ -10,10 +10,16 @@ class Controller:
 
     def get_info_tournament(self) -> None:
         """Get the user entry info and create tournament"""
-
+        while True:
+            if self.check_data_players_numbers() is True:
+                break
         name, place, date, time, description = self.view.ask_info_tournament()
-        tournament = Tournament(name, place, date, time, description)
+        players = self.instantiates_players()
+        tournament = Tournament(name, place, date, players, time, description)
         tournament.save()
+        print("\n The tournament named '{}' has been saved !".format(name))
+        # TEST
+        print(tournament.players["player_1"].first_name)
 
     def get_info_player(self) -> None:
         """Get the user entry info and create a player"""
@@ -39,17 +45,31 @@ class Controller:
                 run = "No"
             if selection == "6":
                 # TEST
-                print(get_field_data("First Name", PLAYERS))
+                self.instantiates_players()
 
-    def check_players_numbers(self) -> None:
+    def check_data_players_numbers(self) -> bool:
         """check if database has 8 minimum players saved in """
         get_players_numbers_saved = 0
         for player in PLAYERS:
             get_players_numbers_saved += 1
-            if get_players_numbers_saved == PLAYERS_NUMBERS:
-                # proposer les joueur dans la database et les instancier p1 p2 ect...
-                pass
-            elif get_players_numbers_saved <= 7:
-                print("minimum players required is 8 !")
-            else:
-                pass
+        if get_players_numbers_saved <= 7:
+            print("The database of players need 8 saved players minimum !")
+            return False
+        elif get_players_numbers_saved == 8:
+            return True
+
+    def instantiates_players(self) -> dict:
+        """instantiates 8 players selected by the user in a dict"""
+
+        players_list = self.view.create_players_id_dict()
+        for key in players_list:
+            player_data = PLAYERS.get(doc_id=players_list.get(key))
+            players_list[key] = Player(
+                first_name=player_data.get("first_name"),
+                last_name=player_data.get("last_name"),
+                birthday=player_data.get("birthday"),
+                genre=player_data.get("genre")
+            )
+        # print(players_list["player_1"].first_name)
+        # print(players_list["player_4"].first_name)
+        return players_list
--- view.py
+++ view.py
@@ -1,6 +1,6 @@
+from string import punctuation, digits
 from data import TOURNAMENTS, PLAYERS
 
-from string import punctuation
 import datetime
 
 
@@ -20,45 +20,34 @@ class View:
 
     def ask_info_tournament(self) -> str:
         """Get info of a tournament"""
-        is_valid = False
-        while is_valid is not True:
-            name = input("Enter the name of the tournament : ").title()
-            is_valid = self.check_input_string(name)
+        print("Enter name of the tournament : ")
+        while True:
+            name = input()
+            if self.check_input_string_special(name) is True:
+                if self.check_input_string_len(name) is True:
+                    if self.check_input_string_integer(name) is True:
+                        break
 
-        is_valid = False
-        while is_valid is not True:
-            place = input("Enter the place of the tournament : ").title()
-            is_valid = self.check_input_string(place)
+        print("Enter place of the tournament : ")
+        while True:
+            place = input()
+            if self.check_input_string_special(place) is True:
+                if self.check_input_string_len(place) is True:
+                    if self.check_input_string_integer(place) is True:
+                        break
 
-        date = input("Enter the date with this format YEAR-MONTH-DAY : ")
-        self.check_date_input(date)
+        print("Enter the date with this format YEAR-MONTH-DAY : ")
+        date = self.check_date_input()
 
-        is_valid = False
-        while is_valid is not True:
-            time = input(
-                "1: Bullet = 1 minute \n"
-                "2: Blitz = 10 minute \n"
-                "3: Rapid = 15 minute \n"
-                "Choose a number to define the time between tour : "
-            )
-            if len(time) >= 2:
-                print("Accept only [1] for bullet and [2] for blitz and [3] for rapid !")
-            elif time == "1":
-                time = 1
-                is_valid = True
-                break
-            elif time == "2":
-                time = 10
-                is_valid = True
-                break
-            elif time == "3":
-                time = 15
-                is_valid = True
-            else:
-                pass
-
+        print(
+            "1: Bullet = 1 minute \n"
+            "2: Blitz = 10 minute \n"
+            "3: Rapid = 15 minute \n"
+            "Choose a number to define the time between tour : "
+        )
+        time = self.request_selection_with_number(1, 10, 15)
         description = input("Enter description of the tournament : ")
-        print("\n The tournament named '{}' has been saved !".format(name))
+
         return name, place, date, time, description
 
     def show_tournaments(self) -> None:
@@ -68,38 +57,33 @@ class View:
 
     def ask_info_player(self) -> str:
         """Get info of a player"""
-        is_valid = False
-        while is_valid is not True:
-            first_name = input("Enter first name : ").title()
-            is_valid = self.check_input_string(first_name)
 
-        is_valid = False
-        while is_valid is not True:
-            last_name = input("Enter last name : ").title()
-            is_valid = self.check_input_string(last_name)
+        print("Enter first name : ")
+        while True:
+            first_name = input()
+            if self.check_input_string_special(first_name) is True:
+                if self.check_input_string_len(first_name) is True:
+                    if self.check_input_string_integer(first_name) is True:
+                        break
 
-        birthday = input("Enter date of birth with this format YEAR-MONTH-DAY :  ")
-        self.check_date_input(birthday)
+        print("Enter last name : ")
+        while True:
+            last_name = input()
+            if self.check_input_string_special(last_name) is True:
+                if self.check_input_string_len(last_name) is True:
+                    if self.check_input_string_integer(last_name) is True:
+                        break
 
-        is_valid = False
-        while is_valid is not True:
-            genre = input(
-                "1: Man \n"
-                "2: Women \n"
-                "Choose a number to define the gender : "
+        print("Enter date of birth with this format YEAR-MONTH-DAY :  ")
+        birthday = self.check_date_input()
+
+        print(
+            "Enter a number for choose the gender : \n"
+            "1 - Man \n"
+            "2 - Women"
             )
-            if len(genre) >= 2:
-                print("Accept only [1] for man and [2] for women !")
-            elif genre == "1":
-                genre = "Man"
-                is_valid = True
-                break
-            elif genre == "2":
-                genre = "Woman"
-                is_valid = True
-                break
-            else:
-                pass
+        genre = self.request_selection_with_number("Man", "Women", "none")
+
         print("\n The player {} {}, {}, birth on {} has been added to the database !".format(
             first_name,
             last_name,
@@ -109,35 +93,98 @@ class View:
         return first_name, last_name, birthday, genre
 
     def show_players(self) -> None:
-        """Show all players in players.json"""
+        """Show all players with id in players.json"""
+        id = 0
+        print("\n")
         for player in PLAYERS:
-            print(player)
+            id += 1
+            print(str(id) + " : " + str(player))
 
-    def check_input_string(self, string_to_check) -> bool:
-        """Check input string entry for not to use special characters and check
-           if the string have 2 character minimum"""
+    def check_input_string_special(self, string_to_check) -> bool:
+        """Check input string entry for not to use special characters"""
         for symbol in punctuation:
             if symbol in string_to_check:
-                is_valid = False
-                print("No special character allowed !")
-                break
-            else:
-                is_valid = True
+                print("No special character allowed : ")
+                return False
+        return True
 
-        if is_valid:
-            while len(string_to_check) < 2:
-                is_valid = False
-                print("The minimum number of characters required is 2")
-                break
-            else:
-                is_valid = True
-        return is_valid
+    def check_input_string_len(self, string_to_check) -> bool:
+        """Check input string entry for enter 2 character minimum"""
+        if len(string_to_check) < 2:
+            print("The minimum number of characters required is 2 : ")
+            return False
+        return True
 
-    def check_date_input(self, date_to_check) -> None:
+    def check_input_string_integer(self, string_to_check) -> bool:
+        """check if string have numbers in, return True or False"""
+        for number in digits:
+            if number in string_to_check:
+                print("No numbers allowed : ")
+                return False
+        return True
+
+    def check_date_input(self) -> str:
         """Check if input date string is a date format"""
         while True:
             try:
-                datetime.datetime.strptime(date_to_check, "%Y-%m-%d")
-                break
+                date = input()
+                datetime.datetime.strptime(date, "%Y-%m-%d")
+                return date
             except ValueError:
-                date_to_check = input("The correct date format is YEAR-MONTH-DAY : ")
+                print("The correct date format is YEAR-MONTH-DAY : ")
+
+    def request_selection_with_number(self, option_1: str or int, option_2: str or int, option_3: str or int) -> str:
+        """
+        Take user integer entry only, and assign string variable:
+        variable = [1] for option_1 | [2] for option_2 | [3] for option_3
+        option_3 is optional enter "none" for delete it.
+        """
+        while True:
+            try:
+                result = int(input())
+            except TypeError:
+                print("Enter a valid number : ")
+            if result == 1:
+                return str(option_1)
+            elif result == 2:
+                return str(option_2)
+            elif result == 3:
+                if option_3 == "none":
+                    print("Enter a valid number : ")
+                else:
+                    return str(option_3)
+            elif len(str(result)) >= 2:
+                print("Enter a valid number : ")
+
+    def create_players_id_dict(self) -> dict:
+        """Request 8 id of players saved in players.json and return dict like this player_1 = id_selected ect"""
+        players_id = {}
+        key = 0
+        self.show_players()
+        print("\n" + "Enter id of wanted players : ")
+        while len(players_id) < 8:
+            while True:
+                id_choice = self.request_players_id()
+                if self.check_not_same_value(players_id, id_choice) is True:
+                    key += 1
+                    players_id["player_{0}".format(str(key))] = id_choice
+                    break
+        # print(players_id)
+        return players_id
+
+    def request_players_id(self) -> int:
+        """request user entry for integer only"""
+        while True:
+            try:
+                id_choice = int(input())
+                return id_choice
+            except ValueError:
+                print("Enter only numbers : ")
+
+    def check_not_same_value(self, players_id: dict, id_choice: int) -> bool:
+        """check if dict contains two same values and return True or false"""
+        for value in players_id.values():
+            if value == id_choice:
+                print("You already selected this player : ")
+                return False
+        return True
