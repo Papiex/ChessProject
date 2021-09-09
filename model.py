0f01b6eccf874386d856e890c0bb002f4db223db
papiex <boutdepapier27@outlook.fr>
2021-09-09 15:49:56 +0200

    Adding repr method for Tournament and Player Class

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ from data import TOURNAMENTS, PLAYERS
 
 class Player:
     """Class defining a player with first and last name, birthday, genre and his ranking"""
-    def __init__(self, first_name, last_name, birthday, genre) -> None:
+    def __init__(self, first_name, last_name, birthday, genre, ranking) -> None:
 
         self.first_name = first_name
         self.last_name = last_name
         self.birthday = birthday
         self.genre = genre
-        self.ranking = 0
+        self.ranking = ranking
 
     def save(self) -> None:
         """Save the info of a player"""
@@ -21,30 +21,43 @@ class Player:
             "ranking": self.ranking
         })
 
+    def __repr__(self):
+        """redifining repr method for print cleaned data"""
+        rep = "{} {} | {} | {} | {}".format(self.first_name, self.last_name, self.birthday, self.genre, self.ranking)
+        return rep
+
 
 class Tournament:
     """Class defining a tournament with name, place, the date, defaut rounds number, current round, list of the
        players, time and description"""
-    def __init__(self, name, place, date, players, time, description) -> None:
+    def __init__(self, name, place, date, time, description) -> None:
 
         self.name = name
         self.place = place
         self.date = date
         self.rounds_number = 4
         self.current_round = 1
-        self.players = players
+        self.players = {}
         self.time = time
         self.description = description
 
     def save(self) -> None:
         """Save the info in tournaments.json"""
         TOURNAMENTS.insert({
-            "Name": self.name,
-            "Place": self.place,
-            "Date": self.date,
-            "Time": self.time,
-            "Description": self.description
+            "name": self.name,
+            "place": self.place,
+            "date": self.date,
+            "players": self.players,
+            "time": self.time,
+            "description": self.description
             })
+
+    def __repr__(self):
+        """"""
+        players = []
+        for player in self.players:
+            players.append(player.first_name)
+        return players
 
 
 class Round:
