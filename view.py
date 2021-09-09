5bc7fe212fe50228e248a7ab607bc8a1b2d13328
papiex <boutdepapier27@outlook.fr>
2021-09-09 15:51:25 +0200

    Adding show_players_specific_tournaments method

--- view.py
+++ view.py
@@ -1,5 +1,8 @@
+from model import Player
 from string import punctuation, digits
 from data import TOURNAMENTS, PLAYERS
+
+import json
 
 import datetime
 
@@ -8,13 +11,14 @@ class View:
 
     def menu(self) -> str:
         """Main menu of the program"""
+        print(" ________________________________________________________________")
         print(
             "\n"
-            "1 - Create a tournament      | 2 - Show all tournaments \n"
-            "3 - Add a player to database | 4 - Show all saved players \n"
-            "5 - Exit the programm        |"
-            "\n"
+            "| 1 - Create a tournament      | 2 - Show all tournaments        |\n"
+            "| 3 - Add a player to database | 4 - Show all saved players      |\n"
+            "| 5 - Exit the programm        | 6 - Add players to a tournament |"
             )
+        print(" ________________________________________________________________")
         response = input("Choose a number to browse the menu : ")
         return response
 
@@ -51,9 +55,17 @@ class View:
         return name, place, date, time, description
 
     def show_tournaments(self) -> None:
-        """Show all the tournaments saved in tournaments.json"""
-        for tournaments in TOURNAMENTS:
-            print(tournaments)
+        """Show all the tournaments with id saved in tournaments.json"""
+        tournament_id = 0
+        print("\n")
+        for tournament in TOURNAMENTS:
+            tournament_id += 1
+            print(
+                str(tournament_id) + " : " + tournament.get("name") + " | " +
+                tournament.get("place") + " | " +
+                tournament.get("date") + " | " +
+                tournament.get("description")
+            )
 
     def ask_info_player(self) -> str:
         """Get info of a player"""
@@ -93,12 +105,63 @@ class View:
         return first_name, last_name, birthday, genre
 
     def show_players(self) -> None:
-        """Show all players with id in players.json"""
-        id = 0
-        print("\n")
+        """Show all players with id in players.json in alphabetical order or not according to the user choice"""
+        players_list = []
         for player in PLAYERS:
-            id += 1
-            print(str(id) + " : " + str(player))
+            data_player = (
+                str(player.get("first_name")) + " " +
+                str(player.get("last_name")) + " | " +
+                str(player.get("birthday")) + " | " +
+                str(player.get("genre"))
+                )
+            players_list.append(data_player)
+
+        print(
+            "Do you want the list of players by alphabetical order ? \n"
+            "1 - Yes \n"
+            "2 - No"
+            )
+        choice = self.request_selection_with_number("Yes", "No", "None")
+        if choice == "Yes":
+            player_id = 0
+            players_list.sort()
+            print("LIST OF ALL PLAYERS IN ALPHABETICAL ORDER : ")
+            for player in players_list:
+                player_id += 1
+                print(str(player_id) + " : " + player)
+        elif choice == "No":
+            player_id = 0
+            print("LIST OF ALL PLAYERS : ")
+            for player in players_list:
+                player_id += 1
+                print(str(player_id) + " : " + player)
+
+    def show_players_specific_tournament(self):
+        """Show player of specific tournament"""
+        self.show_tournaments()
+        id_choice = self.request_id(TOURNAMENTS)
+        tournament_data = TOURNAMENTS.get(doc_id=id_choice)
+        if tournament_data.get("players") == {}:
+            print("\n This tournaments has no players yet")
+        else:
+            players_list = tournament_data.get("players")
+            deserialized_player_list = []
+            for player_data in players_list:
+                deserialized_player = Player(**json.loads(player_data))
+                deserialized_player_list.append(deserialized_player)
+            print(
+                "Do you want the list of players by alphabetical order ? \n"
+                "1 - Yes \n"
+                "2 - No"
+                )
+            choice = self.request_selection_with_number("Yes", "No", "None")
+            if choice == "Yes":
+                deserialized_player_list = sorted(deserialized_player_list, key=lambda player: player.first_name)
+                for deserialized_player in deserialized_player_list:
+                    print(deserialized_player)
+            elif choice == "No":
+                for deserialized_player in deserialized_player_list:
+                    print(deserialized_player)
 
     def check_input_string_special(self, string_to_check) -> bool:
         """Check input string entry for not to use special characters"""
@@ -155,6 +218,8 @@ class View:
                     return str(option_3)
             elif len(str(result)) >= 2:
                 print("Enter a valid number : ")
+            else:
+                print("Enter a valid number : ")
 
     def create_players_id_dict(self) -> dict:
         """Request 8 id of players saved in players.json and return dict like this player_1 = id_selected ect"""
@@ -164,22 +229,30 @@ class View:
         print("\n" + "Enter id of wanted players : ")
         while len(players_id) < 8:
             while True:
-                id_choice = self.request_players_id()
+                id_choice = self.request_id(PLAYERS)
                 if self.check_not_same_value(players_id, id_choice) is True:
                     key += 1
                     players_id["player_{0}".format(str(key))] = id_choice
                     break
-        # print(players_id)
         return players_id
 
-    def request_players_id(self) -> int:
+    def request_id(self, data_file) -> int:
         """request user entry for integer only"""
         while True:
             try:
                 id_choice = int(input())
-                return id_choice
             except ValueError:
                 print("Enter only numbers : ")
+
+            object_counter = 0
+            for object in data_file:
+                object_counter += 1
+            if id_choice > object_counter:
+                print("Select a valid id : ")
+            elif id_choice == 0:
+                print("Select a valid id : ")
+            else:
+                return id_choice
 
     def check_not_same_value(self, players_id: dict, id_choice: int) -> bool:
         """check if dict contains two same values and return True or false"""
