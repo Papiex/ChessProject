d7a9a6c7f6ffd43cc8e19c46596639906d04e251
papiex <boutdepapier27@outlook.fr>
2021-09-18 16:55:17 +0200

    under controller instantiate in the main controller

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from controller import Controller
-from view import View
+from controllers.main_controller import Controller
+from views.main_view import View
+
 
 if __name__ == "__main__":
 
     view = View()
     chess = Controller(view)
-    run = chess.run_menu_selection()
+    run = chess.run_main_menu_selection()
