455c829a45f4860fea5193cedcce8eb5ade4c21c
papiex <boutdepapier27@outlook.fr>
2021-09-27 14:00:30 +0200

    moving deserialize function in tournament model

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -3,9 +3,8 @@ from controllers.player_controller import PlayerController
 from views.player_view import PlayerView
 from data import TOURNAMENTS
 
-from typing import Union
-
-import json
+import check_functions as check
+import utils
 
 from tinydb import where
 
@@ -14,8 +13,8 @@ class TournamentController:
     """Tournament Controller"""
     def __init__(self, view) -> None:
         self.view = view
-        # self.player_view = PlayerView()
-        self.player_controller = PlayerController(PlayerView)
+        self.player_view = PlayerView()
+        self.player_controller = PlayerController(self.player_view)
 
     def get_info_tournament(self) -> None:
         """Get the user entry info and create tournament"""
@@ -25,33 +24,25 @@ class TournamentController:
         name, place, date, time, description = self.view.ask_info_tournament()
         tournament = Tournament(name, place, date, time, description)
         tournament.save()
-        print("\n The tournament named '{}' has been saved !".format(name))
-
-    def instantiate_tournament(self) -> Union[Tournament, int]:
-        """instantiate tournament saved in tournaments.json"""
-        self.view.show_tournaments()
-        tournament_id = self.view.request_id(TOURNAMENTS)
-        tournament_data = TOURNAMENTS.get(doc_id=tournament_id)
-        tournament = Tournament(
-            name=tournament_data.get("name"),
-            place=tournament_data.get("place"),
-            date=tournament_data.get("date"),
-            time=tournament_data.get("time"),
-            description=tournament_data.get("description")
-        )
-        return tournament, tournament_id
 
     def add_tournament_players(self):
-        """Add 8 players to a selected tournament"""
-        tournament, tournament_id = self.instantiate_tournament()
-        # player
-        tournament.players = self.player_controller.instantiates_players()
+        """Add 8 serialized players to a selected tournament"""
+        self.view.show_tournaments()
+        self.view.display_choose_a_tournament()
+        tournament_id = check.request_id(TOURNAMENTS)
+        tournament = Tournament.deserialize_tournament(Tournament, tournament_id)
+        tournament.players, tournament.players_round_id = self.player_controller.instantiates_players()
         tournament_data = TOURNAMENTS.get(doc_id=tournament_id)
         serialized_players_list = []
 
-        for player in tournament.players.values():
-            serialized_player = json.dumps(player.__dict__)
+        for player in tournament.players:
+            serialized_player = player.serialize()
             serialized_players_list.append(serialized_player)
 
         TOURNAMENTS.update({"players": serialized_players_list}, where("name") == tournament_data.get("name"))
-        print("Selected players have been added to the tournament")
+        TOURNAMENTS.update(
+            {"players_round_id": tournament.players_round_id},
+            where("players_round_id") == tournament_data.get("players_round_id"))
+
+        utils.clear_terminal()
+        self.view.display_selected_players(tournament.players)
